f118f337dcdd2e0e0b78b56be32822ba10a1155a
unknown <hatef.dev21@gmail.com>
2025-07-10 20:38:26 +0330

    Wip

--- src/main.js
+++ src/main.js
@@ -4,3 +4,4 @@ import 'swiper/swiper-bundle.css';
 var swiper = new Swiper(".mySwiper", {});
 var swiper2 = new Swiper(".discountSecetion", {})
 var swiper3 = new Swiper(".articleSecetion", {})
+var swiper3 = new Swiper(".videoSection", {})
