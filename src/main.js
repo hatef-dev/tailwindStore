13ca2b2c1de016217bd11ec40bb7e919010f541a
unknown <hatef.dev21@gmail.com>
2025-07-13 23:22:57 +0330

    Wip

--- src/main.js
+++ src/main.js
@@ -5,3 +5,26 @@ var swiper = new Swiper(".mySwiper", {});
 var swiper2 = new Swiper(".discountSecetion", {})
 var swiper3 = new Swiper(".articleSecetion", {})
 var swiper3 = new Swiper(".videoSection", {})
+
+const menu = document.querySelector(".MobileMenu")
+const menuButton = document.querySelector(".MenuButton")
+const closeButton = document.querySelector(".CloseButton")
+const Menubackground = document.querySelector(".Menubackground")
+menuButton.addEventListener("click",()=>{
+  menu.classList.add("right-0")
+  menu.classList.remove("right-[-100%]")
+  Menubackground.classList.add("right-0")
+  Menubackground.classList.remove("right-[-100%]")
+})
+closeButton.addEventListener("click",()=>{
+  menu.classList.remove("right-0")
+  menu.classList.add("right-[-100%]")
+  Menubackground.classList.remove("right-0")
+  Menubackground.classList.add("right-[-100%]")
+})
+Menubackground.addEventListener("click",()=>{
+  menu.classList.remove("right-0")
+  menu.classList.add("right-[-100%]")
+  Menubackground.classList.remove("right-0")
+  Menubackground.classList.add("right-[-100%]")
+})
