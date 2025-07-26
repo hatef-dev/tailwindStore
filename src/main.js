31a94008e333e1264735e42e55be2ccb5ce9cbfd
unknown <hatef.dev21@gmail.com>
2025-07-26 19:36:00 +0330

    WIP

--- src/main.js
+++ src/main.js
@@ -1,10 +1,18 @@
 import Swiper from 'swiper';
 import 'swiper/swiper-bundle.css';
 
-var swiper = new Swiper(".mySwiper", {});
-var swiper2 = new Swiper(".discountSecetion", {})
-var swiper3 = new Swiper(".articleSecetion", {})
-var swiper3 = new Swiper(".videoSection", {})
+var swiper = new Swiper(".mySwiper", {
+  spaceBetween: 40
+});
+var swiper2 = new Swiper(".discountSecetion", {
+  spaceBetween: 40
+})
+var swiper3 = new Swiper(".articleSecetion", {
+  spaceBetween: 40
+})
+var swiper3 = new Swiper(".videoSection", {
+  spaceBetween: 40
+})
 
 const menu = document.querySelector(".MobileMenu")
 const menuButton = document.querySelector(".MenuButton")
