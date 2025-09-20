28649bbb0e8ac71564787d48d958ba1d53338eaf
unknown <hatef.dev21@gmail.com>
2025-09-20 04:19:16 +0330

    add Product

---
+++ src/Product.js
@@ -0,0 +1,44 @@
+import Swiper from "swiper/bundle";
+import "swiper/swiper-bundle.css";
+import "./Product.css";
+// First, create the thumbs swiper
+const swiperThumbs = new Swiper(".swiperThumbs", {
+  spaceBetween: 12,
+  slidesPerView: 4,
+  freeMode: true,
+  watchSlidesProgress: true,
+});
+
+// Then create the main product swiper and reference the thumbs
+const productSwiper = new Swiper(".productShow", {
+  spaceBetween: 10,
+  slidesPerView: 1,
+  centeredSlides: true,
+  thumbs: {
+    swiper: swiperThumbs, // Reference the thumbs swiper here
+  },
+});
+
+const relatedProducts = new Swiper(".relatedProducts", {
+  slidesPerView: 1,
+    slideClass: 'relatedProducts-slider',
+  spaceBetween: 32,
+  breakpoints: {
+    640: { // sm
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    768: { // md
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    1024: { // lg
+      slidesPerView: 4,
+      spaceBetween: 32,
+    },
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
