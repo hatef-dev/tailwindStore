c798992e8d7efac0f99bb04a19ef01bdd92918a4
unknown <hatef.dev21@gmail.com>
2025-09-22 21:10:31 +0330

    add Brands fixes

--- src/Brands/Brands.js
+++ src/Brands/Brands.js
@@ -5,9 +5,21 @@ const activeCols = document.querySelector(".ActiveCols")
 const deActiveCols = document.querySelector(".deActiveCols")
 const deActiveTwoCols = document.querySelector(".deActiveTwoCols")
 const activeTwoCols = document.querySelector(".activeTwoCols")
+const brandImage = document.querySelectorAll(".brandImage")
+const brandContent = document.querySelectorAll(".brandContent")
 oneCols.addEventListener("click", () => {
     countanierContent.classList.remove("grid", "grid-cols-2", "gap-6")
     countanierContent.classList.add("grid", "grid-cols-1", "gap-y-6")
+    // Mark state for one column
+    countanierContent.classList.remove("twoColsActive")
+    brandImage.forEach(image => {
+        image.classList.remove("w-[120px]", "md:w-[180px]", "lg:w-[124px]")
+        image.classList.add("w-[80px]", "md:w-[120px]", "lg:w-[200px]")
+    })
+    brandContent.forEach(content => {
+        content.classList.remove("line-clamp-3")
+        content.classList.add("line-clamp-6", "lg:line-clamp-6")
+    })
     // Show one column active state
     activeCols.classList.remove("hidden")
     activeCols.classList.add("block")
@@ -24,6 +36,16 @@ oneCols.addEventListener("click", () => {
 twoCols.addEventListener("click", () => {
     countanierContent.classList.remove("grid", "grid-cols-1", "gap-y-6")
     countanierContent.classList.add("grid", "grid-cols-2", "gap-6")
+    // Mark state for two columns
+    countanierContent.classList.add("twoColsActive")
+    brandImage.forEach(image => {
+        image.classList.remove("w-[80px]", "md:w-[120px]", "lg:w-[200px]")  
+        image.classList.add("w-[120px]", "md:w-[180px]", "lg:w-[124px]")
+    })
+    brandContent.forEach(content => {
+        content.classList.remove("line-clamp-6", "lg:line-clamp-6")
+        content.classList.add("line-clamp-3")
+    })
     activeCols.classList.remove("block")
     activeCols.classList.add("hidden")
     deActiveCols.classList.remove("hidden")
