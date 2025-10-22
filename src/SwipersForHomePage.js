4d5f0f134686b783e738aaae711c2a52dc1ff249
unknown <hatef.dev21@gmail.com>
2025-10-22 17:43:26 +0330

    fix swipers

--- src/SwipersForHomePage.js
+++ src/SwipersForHomePage.js
@@ -2,20 +2,66 @@ import Swiper from "swiper";
 import "swiper/swiper-bundle.css";
 
 var swiper = new Swiper(".mySwiper", {
-  spaceBetween: 40,
+  spaceBetween: 16,
+  slidesPerView: 1,
+  breakpoints: {
+    480: {
+      slidesPerView: 1.2,
+      spaceBetween: 16,
+    },
+    640: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
 });
 var swiper2 = new Swiper(".discountSecetion", {
-  spaceBetween: 40,
+  spaceBetween: 16,
+  slidesPerView: 1,
+  breakpoints: {
+    480: {
+      slidesPerView: 1.2,
+      spaceBetween: 16,
+    },
+    640: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+    1280: {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+  },
 });
 var swiper3 = new Swiper(".articleSecetion", {
   spaceBetween: 16,
   breakpoints: {
     480: {
-      slidesPerView: 1.3,
+      slidesPerView: 1,
       spaceBetween: 16,
     },
     640: {
-      slidesPerView: 1.6,
+      slidesPerView: 1.5,
       spaceBetween: 20,
     },
     768: {
@@ -32,7 +78,31 @@ var swiper3 = new Swiper(".articleSecetion", {
     },
   },
 });
-var swiper3 = new Swiper(".videoSection", {
-  spaceBetween: 40,
+var swiper4 = new Swiper(".videoSection", {
+  spaceBetween: 16,
+  slidesPerView: 1,
+  centeredSlides: false,
+  breakpoints: {
+    480: {
+      slidesPerView: 1.1,
+      spaceBetween: 16,
+    },
+    640: {
+      slidesPerView: 1.3,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1.8,
+      spaceBetween: 24,
+    },
+    1024: {
+      slidesPerView: 2.5,
+      spaceBetween: 32,
+    },
+    1280: {
+      slidesPerView: "auto",
+      spaceBetween: 40,
+    },
+  },
 });
 
