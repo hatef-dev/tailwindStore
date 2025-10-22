9e08193f610eb5c5b13f0978b0148d5ab4056022
unknown <hatef.dev21@gmail.com>
2025-10-22 13:29:46 +0330

    fix articles swiper

--- src/SwipersForHomePage.js
+++ src/SwipersForHomePage.js
@@ -8,7 +8,29 @@ var swiper2 = new Swiper(".discountSecetion", {
   spaceBetween: 40,
 });
 var swiper3 = new Swiper(".articleSecetion", {
-  spaceBetween: 40,
+  spaceBetween: 16,
+  breakpoints: {
+    480: {
+      slidesPerView: 1.3,
+      spaceBetween: 16,
+    },
+    640: {
+      slidesPerView: 1.6,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
 });
 var swiper3 = new Swiper(".videoSection", {
   spaceBetween: 40,
