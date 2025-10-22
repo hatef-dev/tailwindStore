c5af5f6fbc48535e1e04c0ab19ec94cc006af76a
unknown <hatef.dev21@gmail.com>
2025-10-22 18:56:10 +0330

    fix ShoppingBagFirstStep

--- src/PLP/ArticleSinglePageSwiper.js
+++ src/PLP/ArticleSinglePageSwiper.js
@@ -1,5 +1,27 @@
 import Swiper from "swiper";
 import "swiper/swiper-bundle.css";
 var swiper3 = new Swiper(".articleSecetion", {
-    spaceBetween: 40,
-  });
+  spaceBetween: 16,
+  breakpoints: {
+    480: {
+      slidesPerView: 1,
+      spaceBetween: 16,
+    },
+    640: {
+      slidesPerView: 1.5,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 24,
+    },
+    1024: {
+      slidesPerView: 3,
+      spaceBetween: 24,
+    },
+    1280: {
+      slidesPerView: 3,
+      spaceBetween: 32,
+    },
+  },
+});
